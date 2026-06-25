560ecbaab90b8c8aaf13626d91ee4c86b843b159
zakria tunio <muhammadzakriatunio@gmail.com>
2026-06-25 19:56:06 +0500

    Add URL facade import to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;  // ← THIS LINE IS REQUIRED
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
