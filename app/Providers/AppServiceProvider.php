78e47d43893db4df589b823359d894653a3fb955
zakria tunio <muhammadzakriatunio@gmail.com>
2026-06-25 19:43:11 +0500

    Enforce HTTPS in production
    Force HTTPS scheme in production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+        }
+        
         Schema::defaultStringLength(191);
 
         Paginator::useBootstrap();
